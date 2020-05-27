2dc494fd274683b816323cf29c5e60654f2798f4
TamaraSretkovic <sretkovictamara@gmail.com>
2020-05-27 15:00:20 +0200

    Lexer radi

--- MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -1,4 +1,67 @@
 package rs.ac.bg.etf.pp1;
 
 public class sym {
+    /* key words */
+    public static final int PROGRAM = 1;
+    public static final int STATIC = 2;
+    public static final int CLASS = 3;
+    public static final int ABSTRACT = 4;
+    public static final int EXTENDS = 5;
+    public static final int THIS = 6;
+    public static final int NEW = 7;
+    public static final int RETURN = 8;
+
+    public static final int PRINT = 9;
+    public static final int READ = 10;
+
+    public static final int INT = 11;
+    public static final int CHAR = 12;
+    public static final int BOOL = 13;
+    public static final int VOID = 14;
+    public static final int CONST = 15;
+
+    /* identifiers */
+    public static final int IDENTIFIER = 16;
+
+    /* constants */
+    public static final int NUMBER = 17;
+
+    /* operators */
+    public static final int PLUS = 18;
+    public static final int MINUS = 19;
+    public static final int INC = 20;
+    public static final int DEC = 21;
+    public static final int MUL = 22;
+    public static final int DIV = 23;
+    public static final int MOD = 24;
+    public static final int EQUAL = 25;
+
+    public static final int EQUALS = 26;
+    public static final int NOT_EQUALS = 27;
+    public static final int GR = 28;
+    public static final int GRE = 29;
+    public static final int LE = 30;
+    public static final int LEE = 31;
+    public static final int AND = 32;
+    public static final int OR = 33;
+
+    public static final int COMMA = 34;
+    public static final int SEMI = 35;
+    public static final int DOT = 48;
+    public static final int L_PAREN = 36;
+    public static final int R_PAREN = 37;
+    public static final int L_BRACE = 38;
+    public static final int R_BRACE = 39;
+    public static final int LC_BRACE = 40;
+    public static final int RC_BRACE = 41;
+
+    /* control structures */
+    public static final int IF = 42;
+    public static final int ELSE = 43;
+    public static final int FOR = 44;
+    public static final int BREAK = 45;
+    public static final int CONTINUE = 46;
+
+    /* end of file */
+    public static final int EOF = 47;
 }
